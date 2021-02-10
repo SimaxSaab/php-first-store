1727f8488e20444d6da7137306269cfd1d93ce10
h-9 <minoreat@gmail.com>
2021-02-10 11:07:48 +0200

    Initial with layout

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>First simple store</title>
+  <link rel="stylesheet" href="s.css">
+</head>
+
+<body>
+  <header class="menu">
+    <div class="container">
+      <ul>
+        <li><a href="index.html">Главная</a></li>
+        <li><a href="about.html">о Нас</a></li>
+      </ul>
+    </div>
+  </header>
+
+  <div class="container">
+    <h1>Каталог</h1>
+    <div class="items">
+      <figure>
+        <p><img src="img/corkscrew.jpg" alt="Штопор"></p>
+        <figcaption>Штопор безумный</figcaption>
+        <a href="form.html" class="btn">Купить</a>
+      </figure>
+      <figure>
+        <p><img src="img/sneakers.jpg" alt="КЕды"></p>
+        <figcaption>Кеды-самоеды</figcaption>
+        <a href="form.html" class="btn">Купить</a>
+      </figure>
+      <figure>
+        <p><img src="img/unicorn.jpg" alt="Единорог"></p>
+        <figcaption>Самка единорога</figcaption>
+        <a href="form.html" class="btn">Купить</a>
+      </figure>
+    </div>
+  </div>
+</body>
+
+</html>

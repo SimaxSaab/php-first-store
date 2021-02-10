5816331f86e1fc6e952ba4d57178725e5cdfc0b7
SimaxSaab <minoreat@gmail.com>
2021-02-10 20:19:20 +0200

    Main functionality

--- form.php
+++ form.php
@@ -1,8 +1,19 @@
 <?php
 
 include 'products.php';
+include 'order.php';
 
-if(isset($_GET['id'])) $product = $products[$_GET['id']];
+$product = $products[$_REQUEST['id'] ?? 0];
+$success = false;
+$error = false;
+
+if(!empty($_POST)) {
+  if(empty($_POST['name']) || strlen($_POST['id']) <= 0) {
+    $error = true;
+  } else {
+    $success = makeOrder($product, $_POST['name']);
+  }
+}
 
 ?>
 
@@ -26,23 +37,29 @@ if(isset($_GET['id'])) $product = $products[$_GET['id']];
 
   <div class="container">
     <h1>КорЗина</h1>
-    <p class="success">Осталось чуть-чуть</p>
-    <p class="error">Заполните Ваше Ф.И.О.</p>
-    <div class="wrap-buy">
-      <div class="items">
-      <figure>
-          <p><img src="<?=$product['img'];?>" alt="<?=$product['name'];?>"></p>
-          <figcaption><?=$product['name'];?></figcaption>
-        </figure>   
+    <?php if($success) {?>
+      <p class="success">Ваш заказ оформлен</p>
+    <?php } else { ?>
+      <?php if($error) {?>
+        <p class="error">Заполните Ваше Ф.И.О.</p>
+      <?php }?>
+      <div class="wrap-buy">
+        <div class="items">
+        <figure>
+            <p><img src="<?=$product['img'];?>" alt="<?=$product['name'];?>"></p>
+            <figcaption><?=$product['name'];?></figcaption>
+          </figure>   
+        </div>
+        <form action="/form.php" method="post">
+          <input type="hidden" name="id" value="<?=$product['id'];?>">
+          <label for="">
+            Ф.И.О*:
+            <input type="text" name="name" placeholder="Введите Ваше Ф.И.О.">
+          </label>
+          <input type="submit" class="btn" value="КУПИТЬ">
+        </form>
       </div>
-      <form action="">
-        <label for="">
-          Ф.И.О:
-          <input type="text" name="" id="" placeholder="Введите Ваше Ф.И.О.">
-        </label>
-        <input type="submit" class="btn" value="КУПИТЬ">
-      </form>
-    </div>
+    <?php }?>
   </div>
 </body>
 </html>
--- products.php
+++ products.php
@@ -2,14 +2,17 @@
 
 $products = [
   [
+    'id' => 0,
     'img' => 'img/corkscrew.jpg',
     'name' => 'Штопор безумный'
   ],
   [
+    'id' => 1,
     'img' => 'img/sneakers.jpg',
     'name' => 'Кеды-самоеды'
   ],
   [
+    'id' => 2,
     'img' => 'img/unicorn.jpg',
     'name' => 'Самка единорога'
   ],
